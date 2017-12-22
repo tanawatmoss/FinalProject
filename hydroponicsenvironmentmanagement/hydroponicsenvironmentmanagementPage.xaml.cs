9447c67eb66a7af1aa65e2a79a15cf39770a08bc
Vchrpng Mac <tanawat.piya2@hotmail.com>
2017-12-23 00:03:01 +0700

    first commit

---
+++ hydroponicsenvironmentmanagement/hydroponicsenvironmentmanagementPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace hydroponicsenvironmentmanagement
+{
+    public partial class hydroponicsenvironmentmanagementPage : ContentPage
+    {
+        public hydroponicsenvironmentmanagementPage()
+        {
+            InitializeComponent();
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Navigation.PushAsync(new settings());
+        }
+    }
+}
---
+++ hydroponicsenvironmentmanagement/settings.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace hydroponicsenvironmentmanagement
+{
+    public partial class settings : ContentPage
+    {
+        public settings()
+        {
+            InitializeComponent();
+            InitSwitch();
+        }
+
+        void HandleModeClicked(object sender, System.EventArgs e)
+        {
+
+            if (CurrentMode.Text == "Manual Mode On")
+            {
+                CurrentMode.Text = "Automation Mode On";
+                WaterPump.IsEnabled = false;
+                AirSwitch.IsEnabled = false;
+                LightsSwitch.IsEnabled = false;
+                WaterPumpText.TextColor = Color.LightGray;
+                AirText.TextColor = Color.LightGray;
+                LightsText.TextColor = Color.LightGray;
+
+            }
+            else {
+                WaterPump.IsEnabled = true;
+                AirSwitch.IsEnabled = true;
+                LightsSwitch.IsEnabled = true;
+                CurrentMode.Text = "Manual Mode On";
+                WaterPumpText.TextColor = Color.Black;
+                AirText.TextColor = Color.Black;
+                LightsText.TextColor = Color.Black;
+            }
+
+        }
+
+        void InitSwitch(){
+            WaterPump.Toggled += WaterPump_Toggled;
+        }
+
+        private void WaterPump_Toggled(object sender, ToggledEventArgs e)
+        {
+            DisplayAlert("Toggle","Toggled","cancel");
+        }
+    }
+}
